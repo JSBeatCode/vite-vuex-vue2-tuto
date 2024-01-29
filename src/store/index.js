8586f10dacc9690cbb5c675c0c3f9fc539e8d934
JS <csdnum0@gmail.com>
2024-01-30 00:58:00 +0900

    d

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore } from 'vuex';
 import module1 from './module1';
 import module2 from './module2';
-import main from './main';
+import home from './home';
 
 export default createStore({
     modules : {
         module1,
         module2,
-        main
+        home
     }
 })
